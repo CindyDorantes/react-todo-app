7d7595bd8d699592d84498526f99bac1de6bbb61
Cindy Dorantes <cindym.dorantes@gmail.com>
2022-08-16 14:32:32 -0500

    React lifecycle methods

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -6,6 +6,10 @@ class TodoItem extends React.Component {
     editing: false,
   }
 
+  componentWillUnmount() {
+    console.log("Cleaning up...")
+  }
+
   handleEditing = () => {
     this.setState({
       editing: true,
